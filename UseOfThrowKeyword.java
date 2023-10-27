8b5b0b03a58b573406ea7e320eb684e6a34283bd
Rajesh Gajraj <121161516+rajeshgajraj32@users.noreply.github.com>
2023-10-27 12:45:14 +0530

    ATM project using self creation exception

---
+++ UseOfThrowKeyword.java
@@ -0,0 +1,53 @@
+package com.exceptionHandling;
+
+import java.util.Scanner;
+class MainclssOfBanking extends Exception{
+	MainclssOfBanking(String negAmou){
+		super(negAmou);
+	}
+}
+class NegativeAmount extends MainclssOfBanking{
+	NegativeAmount(String negAmou){
+		super(negAmou);
+	}
+}
+class GreaterThanBalance extends MainclssOfBanking{
+	GreaterThanBalance(String negAmou){
+		super(negAmou);
+	}
+}
+
+public class UseOfThrowKeyword{
+	int balance=2000;
+	void throwException(int amount)throws NegativeAmount,GreaterThanBalance{
+		if(amount<0) {
+			throw new NegativeAmount("["+amount+"]:"+"You Entered negative balance");
+		}
+		else if(amount>balance) {
+			throw new GreaterThanBalance("["+amount+"]:"+"You have not enough balance");
+		}else{
+			balance=balance-amount;
+			System.out.println("withdrawal successfull"+"\n Remining Balance :"+balance);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter withdrawl Amount:");
+		int amount=sc.nextInt();
+		UseOfThrowKeyword uotk = new UseOfThrowKeyword();
+		try {
+			uotk.throwException(amount);
+		} 
+		catch (GreaterThanBalance f) {
+			// TODO: handle exception
+			System.out.println(f.getMessage());
+		}catch (NegativeAmount e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		
+
+	}
+
+}
